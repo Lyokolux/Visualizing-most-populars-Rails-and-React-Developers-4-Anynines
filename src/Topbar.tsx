3c18d027778784778693ed44f9aa8399cde320e8
Lyokolux <lyokolux@protonmail.com>
2020-09-18 15:52:40 +0200

    🚚 move Topbar upward and ♻️ convert scss file to styled component

--- src/Topbar.tsx
+++ src/Topbar.tsx
@@ -1,13 +1,41 @@
 import React, { useState } from 'react';
 import logo from '../assets/logo.svg'
-import './topbar.scss'
+import styled from 'styled-components';
+import './topbar.scss';
 
 import { Lookup } from 'react-rainbow-components';
 import { LookupValue } from 'react-rainbow-components/components/types';
 
-const containerStyles = {
+const LookupInternalStyles = {
   maxWidth: 700,
 };
+
+const StyledHeader = styled.header`
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    font-size: calc(10px + 2vmin);
+    color: white;
+`;
+
+const Logo = styled.img`
+  height: 10vmin;
+  pointer-events: none;
+
+  @media (prefers-reduced-motion: no-preference) {
+    @keyframes App-logo-spin {
+      from {
+        transform: rotate(0deg);
+      }
+      to {
+        transform: rotate(360deg);
+      }
+    }
+
+    animation: App-logo-spin infinite 20s linear;
+  }
+`;
 
 // TODO: use the ones of the HTTP query
 const fakeOptions = [
@@ -25,20 +53,20 @@ const fakeOptions = [
   { label: 'Toronto' },
 ];
 
-const filter = (query: string, options: LookupValue[]): LookupValue[] => {
-  if (query) {
-    const regex = new RegExp(query, 'i');
-    return options.filter(item => {
-      const label = item.label as string;
-      return regex.test(label);
-    });
-  }
-  return [];
-}
-
 const Topbar: React.FC = () => {
   const [value, setValue] = useState<LookupValue>({ label: '' })
   const [options, setOptions] = useState<LookupValue[]>(fakeOptions)
+
+  const filter = (query: string, options: LookupValue[]): LookupValue[] => {
+    if (query) {
+      const regex = new RegExp(query, 'i');
+      return options.filter(item => {
+        const label = item.label as string;
+        return regex.test(label);
+      });
+    }
+    return [];
+  }
 
   const search = (input: string) => {
     const label = value.label as string;
@@ -55,20 +83,20 @@ const Topbar: React.FC = () => {
   }
 
   return (
-    <header className="header">
-      <img src={logo} className="header__logo" alt="logo" />
+    <StyledHeader>
+      <Logo src={logo} alt="logo" />
       <Lookup
         id="lookup-1"
         label="Lookup Label"
-        placeholder="Find"
+        placeholder="Search through"
         options={options}
         value={value}
         onChange={value => setValue({ label: value as string })}
         onSearch={search}
-        style={containerStyles}
+        style={LookupInternalStyles}
         className="rainbow-m-vertical_x-large rainbow-p-horizontal_medium rainbow-m_auto"
       />
-    </header>
+    </StyledHeader>
   );
 }
 
