cfef568a63668056d3b9aac4c99b7b0f1f04f275
Lyokolux <lyokolux@protonmail.com>
2020-09-19 10:55:53 +0200

    🔥 remove or move unused files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Topbar from 'src/Topbar'
+
+const App: React.FC = () => {
+  return (
+    <Topbar />
+  );
+}
+
+export default App
--- src/App/App.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-it('Test are working', () => {
-    expect(true).toBe(true)
-})
