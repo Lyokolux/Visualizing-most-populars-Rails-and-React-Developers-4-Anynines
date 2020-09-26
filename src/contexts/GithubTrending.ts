fee7c9188c09e69c2f2f99ab0c63cf57e9d4e006
Lyokolux <lyokolux@protonmail.com>
2020-09-26 09:32:39 +0200

    ✨(GithubTrending.ts): add the context that store the API results

---
+++ src/contexts/GithubTrending.ts
@@ -0,0 +1,41 @@
+import React from 'react';
+import { fetchDevelopers } from '@huchenme/github-trending';
+
+export type devInTrends = {
+    username: string,
+    name: string,
+    url: string,
+    avatar: string,
+    repo: {
+        name: string,
+        description: string,
+        url: string
+    }
+}
+
+export type GithubTrendingType = {
+    developers: devInTrends[]
+}
+
+const LANGUAGES = ['ruby', 'javascript', 'typescript']
+
+const fetchDeveloppersByLanguage = (languages: string[]): devInTrends[] => {
+    // Send requests
+    const requests: Promise<any>[] = []
+    languages.forEach((language) => {
+        requests.push(fetchDevelopers({ language }) as Promise<devInTrends[]>)
+    })
+
+    // Push the results to one flattened array
+    const allDevelopers: devInTrends[] = [];
+    Promise.all(requests).then((results) =>
+        results.forEach((developpers) => allDevelopers.push(...developpers))
+    )
+    return allDevelopers
+}
+
+const GithubTrending = React.createContext({
+    developers: fetchDeveloppersByLanguage(LANGUAGES)
+})
+
+export default GithubTrending;
