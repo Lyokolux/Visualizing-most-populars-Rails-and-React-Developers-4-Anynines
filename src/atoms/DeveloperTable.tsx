9d245a9c835df977218dced4f7dd91c86492fdb4
Lyokolux <lyokolux@protonmail.com>
2020-09-28 09:34:51 +0200

    ✨(DeveloperTable.tsx): render the avatar image instead of the URL and sort the image

--- src/atoms/DeveloperTable.tsx
+++ src/atoms/DeveloperTable.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
-import { Table, Column } from 'react-rainbow-components';
+import React, { useState, useEffect } from 'react';
+import { Table, Column, Avatar } from 'react-rainbow-components';
+import styled from 'styled-components'
 
-const tableContainerStyles = { height: 300 };
+const TABLE_CONTAINER_STYLES = { height: "50vh" };
+const EMPTY_TABLE_MESSAGE = "Any developers found for this query"
 
 
 type sortDirection = 'asc' | 'desc' | undefined
@@ -10,7 +12,8 @@ type dataObject = {
     username: dataType,
     name: dataType,
     url: dataType,
-    avatar: dataType
+    avatar: dataType,
+    avatarComponent?: React.ReactElement<any, any> | null
 }
 type developerTableData = Array<dataObject>
 
@@ -18,15 +21,47 @@ export type DeveloperTableProps = {
     data: developerTableData
 }
 
+
+/**
+ * ----------------------------------------------------------------------
+ * Component dedicated to render an image into a Column in DeveloperTable
+ * Workaround to avoid unecessary Context
+ */
+const StyledAvatar = styled.div`
+    display: flex;
+    justify-content: left;
+    align-items: space-around;
+    height: 40px;
+`;
+
+const AvatarInDeveloperTable: React.FC<dataObject> = (props) => {
+    return (
+        <StyledAvatar>
+            <Avatar src={props.avatar} assistiveText={`Avatar of ${props.name}`} />
+        </StyledAvatar>
+    )
+}
+// since the attribute "component" of Coulmn can not have a component with props
+// It renders the component first which should be passed to the Column's field attribute
+const avatarUrlToJsx = (data: developerTableData): developerTableData => {
+    return data.map(developer => {
+        developer.avatarComponent = AvatarInDeveloperTable(developer)
+        return developer
+    })
+}
+// ----------------------------------------------------------------------
+
+
 const DeveloperTable: React.FC<DeveloperTableProps> = (props) => {
-    const [data, setData] = useState<developerTableData>(props.data)
-    const [sortedBy, setSortedBy] = useState<string>('username')
+    const [data, setData] = useState<developerTableData>(avatarUrlToJsx(props.data))
+    const [sortedBy, setSortedBy] = useState<string>('name')
     const [sortDirection, setSortDirection] = useState<sortDirection>('asc')
 
-
-    const handleOnSort = (_event: React.MouseEvent<HTMLElement, MouseEvent>, field: string, nextSortDirection: string): void => {
+    const sortRows = (field: string, nextSortDirection: sortDirection): void => {
         let newData = [...data];
 
+        // any type to avoid conflict with dataType as field typed as string is not a valid index
+        // the field typed is determined by the react rainbow library and thus can not be changed
         const key = (x: any): dataType => x[field];
         const reverse = nextSortDirection === 'asc' ? 1 : -1;
 
@@ -35,23 +70,32 @@ const DeveloperTable: React.FC<DeveloperTableProps> = (props) => {
             return reverse * c.localeCompare(d);
         });
 
-        // TODO: replace by states
         setData(sortedData)
         setSortedBy(field)
         setSortDirection(nextSortDirection as sortDirection)
     }
 
+    // Wrapper around sortRows that match the signature for Table's onSort attribute
+    const handleOnSort = (_event: React.MouseEvent<HTMLElement, MouseEvent>, field: string, nextSortDirection: string): void => {
+        sortRows(field, nextSortDirection as sortDirection)
+    }
+
+    // Sort the table on first render only
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(() => sortRows(sortedBy, sortDirection), [])
+
     return (
         <div className="rainbow-p-bottom_xx-large">
-            <div style={tableContainerStyles}>
+            <div style={TABLE_CONTAINER_STYLES}>
                 <Table
                     keyField="id"
                     data={data}
                     onSort={handleOnSort}
                     sortDirection={sortDirection}
                     sortedBy={sortedBy}
+                    emptyTitle={EMPTY_TABLE_MESSAGE}
                 >
-                    <Column header="Avatar" field="avatar" />
+                    <Column header="Avatar" field="avatarComponent" width="100" />
                     <Column header="Username" field="username" sortable />
                     <Column header="Name" field="name" sortable />
                     <Column header="URL" field="url" />
