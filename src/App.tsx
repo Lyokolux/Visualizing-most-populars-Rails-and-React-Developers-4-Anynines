dd08b37cdd7d57cf8a6261433884969969b649fc
Lyokolux <lyokolux@protonmail.com>
2020-09-28 10:15:02 +0200

    🐛(App.tsx): fix the constant request sent and rerender

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,9 @@ const App: React.FC = () => {
 
   const API_NOT_LOADED_STATE: GithubTrendingType = []
   const [apiResponse, setApiResponse] = useState<GithubTrendingType>(API_NOT_LOADED_STATE);
+  useEffect(() => {
   fetchAPI().then((results) => setApiResponse(results))
+  }, [])
 
   if (apiResponse === API_NOT_LOADED_STATE)
     return (
